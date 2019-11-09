442a98e2d9a71defb236b2febe64bd1f0aa7b3bb
Adam Cooper <adam@sequence7.net>
2019-11-09 18:16:32 +0100

    platform should be available now

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,15 @@ import { wait } from './wait'
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    const token = core.getInput('token');
 
     // Download the latest platform CLI release
     await exec.exec("curl -L https://github.com/platformsh/platformsh-cli/releases/latest/download/platform.phar  -o platform");
     // Make it executable
     await exec.exec("chmod +x platform");
-    // Move it so it's available 
+    // Move it so it to the user's /bin 
     await exec.exec("sudo mv platform /usr/local/bin/platform");
-    // try something
-    await exec.exec("platform list");
 
-
-    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
