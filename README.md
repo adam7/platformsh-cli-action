6260a2abe3ca57ffed60cb8dcc8359d5ecf6dd1f
Adam Cooper <adam@sequence7.net>
2019-11-10 09:45:54 +0100

    Prepare for launch 👩‍🚀

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
 # Platform.sh Action
-A GitHub Action that allows you to use the Platform.sh CLI in your workflows. 
+
+[![Actions Status](https://github.com/adam7/platformsh-cli-action/workflows/Test%20workflow/badge.svg)](https://github.com/adam7/platformsh-cli-action/actions)
+
+This Action sets up an environment with the Platform.sh CLI. 
 
 ## Inputs
 
-### 'token'
-
+### token
 **Required** Your platform.sh API token, see https://docs.platform.sh/development/cli/api-tokens.html
 
-## Example usage
+### version
 
-    uses: adam7/platformsh-action
+**Optional** Your platform.sh API token, see https://docs.platform.sh/development/cli/api-tokens.html
+
+## Usage
+
+See [action.yml](action.yml)
+
+Basic:
+
+    - uses: actions/checkout@master
+    - uses: adam7/platformsh-action@master
       with: 
         token: ${{secrets.PLATFORMSH_CLI_TOKEN}} #required
+
+Specify CLI version:
+
+    - uses: actions/checkout@master
+      - uses: adam7/platformsh-action@master
+        with: 
+          token: ${{secrets.PLATFORMSH_CLI_TOKEN}} #required
+          version: v3.49.3
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-import { wait } from './wait'
 
 async function run() {
   try {
     const token = core.getInput('token');
+    const version = core.getInput('version');
 
     // Download the latest platform CLI release
-    await exec.exec("curl -L https://github.com/platformsh/platformsh-cli/releases/latest/download/platform.phar  -o platform");
+    await exec.exec(`curl -L ${buildUrl(version)}  -o platform`);
     // Make it executable
     await exec.exec("chmod +x platform");
-    // Move it so it to the user's /bin 
+    // Move it to the user's /bin 
     await exec.exec("sudo mv platform /usr/local/bin/platform");
 
     // Set the platform token as a top level environment variable
-    // await exec.exec(`sudo export PLATFORMSH_CLI_TOKEN="${token}"`);
     core.exportVariable('PLATFORMSH_CLI_TOKEN', token);
+
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function buildUrl(version: String):String{
+  const rootUrl = "https://github.com/platformsh/platformsh-cli/releases/";
+
+  return `${rootUrl}${version}/platform.phar`;
 }
 
 run();
