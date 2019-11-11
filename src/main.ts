85225514109ccb990f92faa32af6e260ee977c5b
Adam Cooper <adam@sequence7.net>
2019-11-11 18:14:11 +0100

    Try using actions toolkit instead of bash

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+// import * as exec from '@actions/exec';
+import * as toolsCache from '@actions/tool-cache';
+import * as io from '@actions/io';
 
 async function run() {
   try {
@@ -9,12 +11,17 @@ async function run() {
     const url = buildUrl(version);
 
     // Download the latest platform CLI release
-    await exec.exec(`curl -L ${url} -o platform`);
+    // await exec.exec(`curl -L ${url} -o platform`);
+    toolsCache.downloadTool(url);
+        
     // Make it executable
-    await exec.exec("chmod +x platform");
+    // await exec.exec("chmod +x platform");
+    
     // Move it to the user's /bin 
-    await exec.exec("sudo mv platform /usr/local/bin/platform");
-
+    // await exec.exec("sudo mv platform /usr/local/bin/platform");
+    
+    io.mv("platform.phar", "/usr/local/bin/platform");
+    
     // Set the platform token as a top level environment variable
     core.exportVariable('PLATFORMSH_CLI_TOKEN', token);
 
