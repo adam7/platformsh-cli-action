8d9e51e6393034248f3c261505fa4d06b7dc62c3
Adam Cooper <mail@admcpr.com>
2019-11-12 09:10:29 +0100

    Switch back to bash, the toolkit doesn't work as expected

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
-import * as toolsCache from '@actions/tool-cache';
-import * as io from '@actions/io';
+// import * as toolsCache from '@actions/tool-cache';
+// import * as io from '@actions/io';
 
 async function run() {
   try {
@@ -11,16 +11,15 @@ async function run() {
     const url = buildUrl(version);
 
     // Download the latest platform CLI release
-    // await exec.exec(`curl -L ${url} -o platform`);
-    const platformPath = await toolsCache.downloadTool(url);
+    await exec.exec(`curl -L ${url} -o platform`);
+    // const platformPath = await toolsCache.downloadTool(url);
         
     // Make it executable
-    // await exec.exec("chmod +x platform");
+    await exec.exec("chmod +x platform");
     
     // Move it to the user's /bin 
-    // await exec.exec("sudo mv platform /usr/local/bin/platform");
-    
-    await io.mv(platformPath, "/usr/local/bin/platform");
+    await exec.exec("sudo mv platform /usr/local/bin/platform");
+    // await io.mv(platformPath, "/usr/local/bin/platform");
     
     // Set the platform token as a top level environment variable
     core.exportVariable('PLATFORMSH_CLI_TOKEN', token);
