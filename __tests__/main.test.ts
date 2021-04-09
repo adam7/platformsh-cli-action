f80dc92729a89131084a35ee9768117a82feb330
Adam Cooper <mail@admcpr.com>
2021-04-09 20:10:39 +0000

    Prettier 🦋

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,16 +1,15 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import * as process from "process";
+import * as cp from "child_process";
+import * as path from "path";
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-    process.env['PLATFORMSH_CLI_TOKEN'] = 'latest';
-    const ip = path.join(__dirname, '..', 'lib', 'main.js');
-    const options: cp.ExecSyncOptions = {
-        env: process.env
-    };
+test("test runs", () => {
+  process.env["PLATFORMSH_CLI_TOKEN"] = "latest";
+  const ip = path.join(__dirname, "..", "lib", "main.js");
+  const options: cp.ExecSyncOptions = {
+    env: process.env,
+  };
 
-    // TODO: Test running the action - no more curl etc.
-    // console.log(cp.execSync(`node ${ip}`, options).toString());
-
+  // TODO: Test running the action - no more curl etc.
+  // console.log(cp.execSync(`node ${ip}`, options).toString());
 });
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,34 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
 // import * as toolsCache from '@actions/tool-cache';
 // import * as io from '@actions/io';
 
 async function run() {
   try {
-    const token = core.getInput('token');
-    const version = core.getInput('version');
+    const token = core.getInput("token");
+    const version = core.getInput("version");
 
     const url = buildUrl(version);
 
     // Download the latest platform CLI release
     await exec.exec(`curl -L ${url} -o platform`);
     // const platformPath = await toolsCache.downloadTool(url);
-        
+
     // Make it executable
     await exec.exec("chmod +x platform");
-    
-    // Move it to the user's /bin 
+
+    // Move it to the user's /bin
     await exec.exec("sudo mv platform /usr/local/bin/platform");
     // await io.mv(platformPath, "/usr/local/bin/platform");
-    
-    // Set the platform token as a top level environment variable
-    core.exportVariable('PLATFORMSH_CLI_TOKEN', token);
 
+    // Set the platform token as a top level environment variable
+    core.exportVariable("PLATFORMSH_CLI_TOKEN", token);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-function buildUrl(version: string):string{
+function buildUrl(version: string): string {
   const rootUrl = "https://github.com/platformsh/platformsh-cli/releases/";
 
   return `${rootUrl}${version}/download/platform.phar`;
